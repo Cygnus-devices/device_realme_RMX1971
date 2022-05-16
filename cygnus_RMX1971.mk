dcca3b935e454de582fa73a97a91ada6658ce941
MadhavSaladi <baaswanthmadhav@gmail.com>
2022-05-16 10:09:34 +0000

    RMX1971: Init Cygnus

--- cygnus_RMX1971.mk
+++ cygnus_RMX1971.mk
@@ -4,18 +4,19 @@
 # SPDX-License-Identifer: Apache-2.0
 #
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Cygnus stuff.
+$(call inherit-product, vendor/cygnus/config/common_full_phone.mk)
 
 # Inherit from RMX1971 device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
+WITH_GAPPS := true
+CYGNUS_BUILD_TYPE := OFFICIAL
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := RMX1971
-PRODUCT_NAME := aosp_RMX1971
+PRODUCT_NAME := cygnus_RMX1971
 PRODUCT_BRAND := Realme
 PRODUCT_MODEL := Realme 5 Pro
 PRODUCT_MANUFACTURER := Realme
